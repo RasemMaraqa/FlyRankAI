dbd3b18a30b962b7605104eab08963de364dc8b2
rasem|maraqa <rasemmaraqa@gmail.com>
2026-07-25 12:56:27 +0300

    read endpoints with 404
    added tasks Endpoint , trying to use HTTPException Next commit

--- main.py
+++ main.py
@@ -3,7 +3,29 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-'''init the first fastapi server'''
+tasks = [
+        {
+            "id": 1,
+            "title": "Start Journaling",
+            "done": True
+        },
+        {
+            "id": 2,
+            "title": "Study for The Final",
+            "done": False
+        },
+        {
+            "id": 3,
+            "title": "Talk with my Therpist ",
+            "done": False
+        },
+        {
+            "id": 4,
+            "title": "Use HTTPException Next Commit",
+            "done": False
+        }
+    ]
+
 
 @app.get("/")
 def read_root():
@@ -17,3 +39,22 @@ def read_root():
 def health_check():
     '''health check endpoint'''
     return {"status": "ok"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    '''get a All Tasks'''
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    '''get a single Task'''
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    return {
+        "error": f"Task {task_id} not found",
+        "status code": 404
+    }
