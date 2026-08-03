ddebd50ea4afc4c8c283427c60bbc27a5e87df37
rasem|maraqa <rasemmaraqa@gmail.com>
2026-08-03 04:54:38 +0300

    Stage 4: explored SQLite

--- main.py
+++ main.py
@@ -40,17 +40,19 @@ def init_db():
 def seed_db():
     conn = get_db()
     cursor = conn.cursor()
-
+    next_id = get_next_available_id()
     cursor.execute("SELECT COUNT(*) FROM tasks")
     count = cursor.fetchone()[0]
 
     if count == 0:
-        cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)",
-                       ("Learn FastAPI", 1))
-        cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)",
-                       ("Build API", 0))
-        cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)",
-                       ("Push to GitHub", 1))
+        cursor.execute("INSERT INTO tasks (id, title, done) VALUES (?,?, ?)",
+                       (next_id, "Learn FastAPI", 1))
+        next_id += 1
+        cursor.execute("INSERT INTO tasks (id, title, done) VALUES (?,?, ?)",
+                       (next_id, "Build API", 0))
+        next_id += 1
+        cursor.execute("INSERT INTO tasks (id, title, done) VALUES (?,?, ?)",
+                       (next_id, "Push to GitHub", 1))
 
     conn.commit()
     conn.close()
@@ -59,7 +61,26 @@ def seed_db():
 @app.on_event("startup")
 def startup():
     init_db()
-    seed_db()
+    
+
+
+
+def get_next_available_id():
+    conn = get_db()
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT id FROM tasks ORDER BY id ASC")
+    existing_ids = [row[0] for row in cursor.fetchall()]
+    conn.close()
+
+    candidate_id = 1
+    for current_id in existing_ids:
+        if current_id == candidate_id:
+            candidate_id += 1
+        elif current_id > candidate_id:
+            break
+
+    return candidate_id
 
 
 @app.get("/")
@@ -110,13 +131,16 @@ def create_task(body: Task):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Title cannot be empty"
         )
+
+    next_id = get_next_available_id()
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO tasks (title, done) VALUES (?, ?)", (title, 0))
+    cursor.execute("INSERT INTO tasks (id, title, done) VALUES (?,?, ?)",
+                   (next_id, title, 0))
     conn.commit()
-    new_id = cursor.lastrowid
+
     new_task = conn.execute("SELECT * FROM tasks WHERE id = ?",
-                            (new_id,)).fetchone()
+                            (next_id,)).fetchone()
     conn.close()
 
     return dict(new_task)
